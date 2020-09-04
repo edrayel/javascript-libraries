9d333fcef2e18934843da7b05c9c1a25fdfd335e
edrayel <edrayel@outlook.com>
2020-09-04 01:38:56 +0100

    Create Prime Number Libraries

---
+++ arrayPrimeNumbers.js
@@ -0,0 +1,41 @@
+function isPrime(val) {
+    for (let i = 2; i < val; i++) {
+        if (val % i === 0) {
+            return false;
+        }
+    }
+    return val > 1;
+}
+
+function primeNumbers(arr) {
+    isArray = false;
+    negativeArray = false;
+    primeNumbersArray = [];
+
+    if (Array.isArray(arr)) {
+        isArray = true;
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                negativeArray = true;
+                console.log('Array must contain positive numbers only.');
+                break;
+            } else {
+                if (Number.isInteger(arr[i])) {
+                    if (isPrime(arr[i])) {
+                        primeNumbersArray.push(arr[i]);
+                    } else {
+                        console.log(arr[i] + ' is not a prime number.');
+                    }
+                } else {
+                    console.log('Array contains non-integers.');
+                    break;
+                }
+            }
+        }
+    } else {
+        isArray = false;
+        console.log('Argument is not an array.');
+    }
+
+    return primeNumbersArray;
+}
